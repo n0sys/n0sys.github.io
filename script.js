409f4cd8f5f4733367c09e6ea26972a39bb29cb5
Hanna Nassar <hannanassar1999@gmail.com>
2024-01-25 12:16:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-document.location.assign("https://webhook.site/9d69f312-1eda-4197-8591-5716250d09fd");
+document.location.assign("https://webhook.site/9d69f312-1eda-4197-8591-5716250d09fd?updates=True");
